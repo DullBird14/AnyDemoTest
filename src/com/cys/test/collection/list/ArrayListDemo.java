7f64efe90bb84d5046eb6a241e836829e0fd414f
DullBird14 <524318257@qq.com>
2018-02-28 22:57:13 +0800

    集合框架练习

---
+++ src/com/cys/test/collection/list/ArrayListDemo.java
@@ -0,0 +1,11 @@
+package com.cys.test.collection.list;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+        int i = Integer.MAX_VALUE+1;
+        System.out.println(i);
+    }
+}
